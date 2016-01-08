6fa1c9999dbc094f4c6cc2f4376c8e86e72f46f1
lstephen <levi.stephen@gmail.com>
2016-01-07 16:00:55 -0800

    Updates for new construi

---
+++ construi/run/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+printf "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+cp /ssh/id_rsa /root/.ssh/id_rsa
+chmod 0600 /root/.ssh/id_rsa
+
+if [ -d "esms-ai-data/.git" ]; then
+  echo "Pulling latest data..."
+  cd esms-ai-data
+  git reset --hard HEAD
+  git clean -fd || true
+  git pull --rebase
+  cd ..
+else
+  echo "Cloning latest data..."
+  rm -rf esms-ai-data
+  git clone git@github.com:lstephen/esms-ai-data.git
+fi
+
+echo "Running..."
+mvn -B install exec:java -Dgpg.skip=true
+
+echo "Updating data..."
+cd esms-ai-data
+
+[[ -n "$GIT_AUTHOR_NAME" ]] && git config user.name $GIT_AUTHOR_NAME
+[[ -n "$GIT_AUTHOR_EMAIL" ]] && git config user.email $GIT_AUTHOR_EMAIL
+
+git add --all
+git commit -m "$(date)"
+git pull --rebase
+git push origin master
+
+echo "Done."
+
