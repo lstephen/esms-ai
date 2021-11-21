e1b4b2dd6ad404ea576e80c551d163f9e9b0868c
Levi Stephen <levi.stephen@gmail.com>
2021-11-21 08:28:34 -0800

    Update SSL formation rules

--- src/main/java/com/ljs/ifootballmanager/ai/formation/validate/FormationValidatorFactory.java
+++ src/main/java/com/ljs/ifootballmanager/ai/formation/validate/FormationValidatorFactory.java
@@ -11,10 +11,10 @@ public final class FormationValidatorFactory {
     return CountingFormationValidator.builder()
         .exactly(1, Role.GK)
         .range(3, 5, Role.DF)
-        .range(2, 6, Role.DM, Role.MF, Role.AM)
+        .range(1, 6, Role.DM, Role.MF, Role.AM)
         .max(3, Role.DM)
         .max(3, Role.AM)
-        .range(1, 4, Role.FW)
+        .range(0, 4, Role.FW)
         .build();
   }
 
